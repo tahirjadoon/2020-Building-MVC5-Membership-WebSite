b3cf2b38fa0db188c47eb7f54471e58ef4158d7f
Tahir Jadoon <tjadoon@gmail.com>
2020-04-21 03:02:26 -0400

    Initial push

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 "# 2020-Building-MVC5-Membership-WebSite" 
+
+A sample website. 
+
+Check App_Docs for more details. 
---
+++ Work/WebApp/Web.Memberships/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Web.Memberships.Startup))]
+namespace Web.Memberships
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

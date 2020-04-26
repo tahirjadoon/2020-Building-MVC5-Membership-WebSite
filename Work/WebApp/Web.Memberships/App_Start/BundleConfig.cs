e69badeaef6317c17f0eb581f629152941cf7f91
Tahir Jadoon <tjadoon@gmail.com>
2020-04-26 00:56:48 -0400

    Creating AdminMenu partial file Adding the AdminMenu partial file to _Layout.cshtml Creating the javascript file to toggle the menu to open and show when hovering over it. Adding the newly create js file to the site specific bundle in BundleConfig and Fianally adding the newly created bundle to the _layout.cshtml file.

--- Work/WebApp/Web.Memberships/App_Start/BundleConfig.cs
+++ Work/WebApp/Web.Memberships/App_Start/BundleConfig.cs
@@ -25,6 +25,10 @@ namespace Web.Memberships
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/sitejs").Include(
+                "~/Scripts/SiteAdminMenu.js"
+                ));
         }
     }
 }
---
+++ Work/WebApp/Web.Memberships/Scripts/SiteAdminMenu.js
@@ -0,0 +1,7 @@
+﻿$(function () {
+    //target the li that has the attribute data-admin-menu
+    //we'll toggle the class open, to open and close the menu
+    $('li[data-admin-menu]').hover(function () {
+        $(this).toggleClass('open');
+    });
+});

b204d0d8ff0a85837e854913ab9e5b35cfd72682
Tahir Jadoon <tjadoon@gmail.com>
2020-04-27 02:12:07 -0400

    SCaffolding Item Entity and Creating Extension methods to help with dropdowns in MVC

---
+++ Work/WebApp/Web.Memberships/Extensions/ICollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Memberships.Extensions
+{
+    public static class ICollectionExtensions
+    {
+        /// <summary>
+        /// Converts ICollection<T> to IEnumarable<SelectListItem>. We need this to display items in dropdowns in MVC views
+        /// </summary>
+        /// <typeparam name="T">The ICollection T to display in the dropdown</typeparam>
+        /// <param name="items">The ICollection T items to display in the dropdown</param>
+        /// <param name="selectedValue">The selectedValue in the dropdown</param>
+        /// <returns>IEnumerable of SelectListItem</returns>
+        public static IEnumerable<SelectListItem> ToSelectListItem<T>(this ICollection<T> items, int selectedValue)
+        {
+            if (items == null) return null;
+
+            var selectListItems = items.Select(x => new SelectListItem()
+            {
+                Text = x.GetPropertyValue("Title"),
+                Value = x.GetPropertyValue("Id"), 
+                Selected = x.GetPropertyValue("Id").Equals(selectedValue.ToString())
+            });
+
+            return selectListItems;
+        }
+    }
+}
---
+++ Work/WebApp/Web.Memberships/Extensions/ReflectionExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Memberships.Extensions
+{
+    public static class ReflectionExtensions
+    {
+        /// <summary>
+        /// Checks if the property exists in type T
+        /// </summary>
+        /// <typeparam name="T">The type</typeparam>
+        /// <param name="item">The T item</param>
+        /// <param name="propertyName">The propertyName in T</param>
+        /// <returns>bool</returns>
+        public static bool IsPropertyExists<T>(this T item, string propertyName)
+        {
+            //must have the basics
+            if (string.IsNullOrWhiteSpace(propertyName) || item == null) return false;
+
+            //property name exists
+            var property = item.GetType().GetProperty(propertyName);
+            if (property == null) return false;
+
+            return true;
+        }
+
+        /// <summary>
+        /// Extension method to get the property value from T using reflections
+        /// </summary>
+        /// <typeparam name="T">The type</typeparam>
+        /// <param name="item">The T item</param>
+        /// <param name="propertyName">The propertyName in T whose value needs to be fetched</param>
+        /// <returns>string</returns>
+        public static string GetPropertyValue<T>(this T item, string propertyName)
+        {
+            //must have the basics
+            if (!item.IsPropertyExists(propertyName)) return "";
+
+            var value = item.GetType()
+                            .GetProperty(propertyName)
+                            .GetValue(item, null)
+                            .ToString();
+            return value; 
+        }
+    }
+}

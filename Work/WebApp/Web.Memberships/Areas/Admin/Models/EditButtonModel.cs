99f17485bab54c98ec523f797bf59ffe263a20b9
Tahir Jadoon <tjadoon@gmail.com>
2020-04-26 23:34:17 -0400

    Creating buttons partial views and scaffolding section entity

---
+++ Work/WebApp/Web.Memberships/Areas/Admin/Models/EditButtonModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace Web.Memberships.Areas.Admin.Models
+{
+    public class EditButtonModel
+    {
+        public int ItemId { get; set; }
+        public int ProductId { get; set; }
+        public int SubscriptionId { get; set; }
+        public string Link
+        {
+            get
+            {
+                var s = new StringBuilder("?");
+                if (ItemId > 0) s.Append($"itemId={ItemId}&");
+                if (ProductId > 0) s.Append($"productId={ProductId}&");
+                if (SubscriptionId > 0) s.Append($"subscriptionId={SubscriptionId}&");
+                return s.ToString().Substring(0, s.Length - 1);
+            }
+        }
+    }
+}
---
+++ Work/WebApp/Web.Memberships/Models/SmallButtonModel.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace Web.Memberships.Models
+{
+    public class SmallButtonModel
+    {
+        public string Action { get; set; }
+        public string Text { get; set; }
+        public string Glyph { get; set; }
+        public string ButtonType { get; set; }
+        public int? Id { get; set; }
+        public int? ItemId { get; set; }
+        public int? ProductId { get; set; }
+        public int? SubscriptionId { get; set; }
+        public string UserId { get; set; }
+
+        public string ActionParameters
+        {
+            get
+            {
+                var param = new StringBuilder("?");
+                if (Id != null && Id > 0)
+                    param.Append($"id={Id}&");
+
+                if (ItemId != null && ItemId > 0)
+                    param.Append($"itemId={ItemId}&");
+
+                if (ProductId != null && ProductId > 0)
+                    param.Append($"productId={ProductId}&");
+
+                if (SubscriptionId != null && SubscriptionId > 0)
+                    param.Append($"subscriptionId={SubscriptionId}&");
+
+                if (UserId != null && !UserId.Equals(string.Empty))
+                    param.Append($"userId={UserId}&");
+
+                return param.ToString().Substring(0, param.Length - 1);
+            }
+        }
+    }
+}

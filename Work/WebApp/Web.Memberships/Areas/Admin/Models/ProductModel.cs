a20ce1958cdec803a5320a2f8890423a2e99e84d
Tahir Jadoon <tjadoon@gmail.com>
2020-04-27 23:34:37 -0400

    Adding ProductModel and ConversionExtensions for product

---
+++ Work/WebApp/Web.Memberships/Areas/Admin/Models/ProductModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Web.Memberships.Entities;
+
+namespace Web.Memberships.Areas.Admin.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+
+        [MaxLength(255)]
+        [Required]
+        public string Title { get; set; }
+
+        [MaxLength(2048)]
+        public string Description { get; set; }
+
+        [MaxLength(1024)]
+        [DisplayName("Image Url")]
+        public string ImageUrl { get; set; }
+
+        public int ProductLinkTextId { get; set; }
+
+        public int ProductTypeId { get; set; }
+
+        [DisplayName("Product Link Text")]
+        public ICollection<ProductLinkText> ProductLinkTexts { get; set; }
+
+        [DisplayName("Product Type")]
+        public ICollection<ProductType> ProductTypes { get; set; }
+
+        public string ProductType => ProductTypes == null || !ProductTypes.Any() ? String.Empty : ProductTypes.First(pt => pt.Id.Equals(ProductTypeId)).Title; 
+        
+        public string ProductLinkText => ProductLinkTexts == null || !ProductLinkTexts.Any() ? String.Empty : ProductLinkTexts.First(pt => pt.Id.Equals(ProductLinkTextId)).Title;
+    }
+}
---
+++ Work/WebApp/Web.Memberships/Extensions/ConversionExtensions.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Web.Memberships.Areas.Admin.Models;
+using Web.Memberships.Entities;
+using Web.Memberships.Models;
+
+namespace Web.Memberships.Extensions
+{
+    public static class ConversionExtensions
+    {
+        #region Prduct
+        public static async Task<IEnumerable<ProductModel>> Convert(this IEnumerable<Product> products, ApplicationDbContext db)
+        {
+            if (products == null || !products.Any())
+                return new List<ProductModel>();
+
+            var texts = await db.ProductLinkTexts.ToListAsync();
+            var types = await db.ProductTypes.ToListAsync();
+
+            var newProducts = products.Select(p => new ProductModel() 
+            {
+                Id = p.Id,
+                Title = p.Title,
+                Description = p.Description,
+                ImageUrl = p.ImageUrl,
+                ProductLinkTextId = p.ProductLinkTextId,
+                ProductTypeId = p.ProductTypeId,
+                ProductLinkTexts = texts,
+                ProductTypes = types
+            });
+
+            return newProducts;
+        }
+
+        public static async Task<ProductModel> Convert(this Product product, ApplicationDbContext db)
+        {
+            if (product == null)
+                return new ProductModel();
+
+            var text = await db.ProductLinkTexts.FirstOrDefaultAsync(p => p.Id.Equals(product.ProductLinkTextId));
+            var type = await db.ProductTypes.FirstOrDefaultAsync(p => p.Id.Equals(product.ProductTypeId));
+
+            var model = new ProductModel
+            {
+                Id = product.Id,
+                Title = product.Title,
+                Description = product.Description,
+                ImageUrl = product.ImageUrl,
+                ProductLinkTextId = product.ProductLinkTextId,
+                ProductTypeId = product.ProductTypeId,
+                ProductLinkTexts = new List<ProductLinkText>(),
+                ProductTypes = new List<ProductType>()
+            };
+
+            model.ProductLinkTexts.Add(text);
+            model.ProductTypes.Add(type);
+
+            return model;
+        }
+
+        #endregion
+    }
+}
